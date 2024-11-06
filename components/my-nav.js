bbd6992bd6d1196bd9ae65c582393f030efb6ac6
Marc Frankfurter <marc@salenet.ag>
2024-11-06 15:39:54 +0100

    progress

--- components/my-nav.js
+++ components/my-nav.js
@@ -2,11 +2,11 @@ class MyNav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <nav class="sticky top-0 bg-gray-900 shadow-lg z-50">
-        <div class="container mx-auto px-4 py-4">
-          <div class="flex justify-start space-x-8">
+        <div class="container mx-auto px-4 max-w-7xl">
+          <div class="flex items-center justify-start space-x-8 py-4">
             <a href="index.html#home" class="flex items-center">
-              <div class="w-6 h-6  overflow-hidden bg-gray-900 flex items-center justify-center">
-                <img src="public/logo/fr-logo.png" alt="Logo" class="w-6 h-6">
+              <div class="w-8 h-8 overflow-hidden bg-gray-900 flex items-center justify-center">
+                <img src="public/logo/fr-logo.png" alt="Logo" class="w-full h-full object-contain">
               </div>
             </a>
             <a href="index.html#about" class="text-gray-300 hover:text-white transition-colors">About</a>
